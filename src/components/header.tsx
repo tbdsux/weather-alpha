e85d5dfe932007d3a7da960ac162384d5a38f73e
TheBoringDude <iamcoderx@gmail.com>
2021-06-24 21:00:19 +0800

    minor fixes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="pt-20 pb-12 text-center">
       <Link href="/">
         <a className="">
-          <h1 className="text-2xl tracking-wide font-black text-gray-700">
+          <h1 className="text-2xl tracking-wide font-extrabold text-gray-700">
             weather <span className="text-pink-500">alpha</span>
           </h1>
         </a>
--- src/modules/weather/current.tsx
+++ src/modules/weather/current.tsx
@@ -24,26 +24,26 @@ const CurrentWeather = ({ geo }: CurrentWeatherProps) => {
   if (!data) return <LoadingComponent />;
 
   return (
-    <div className="p-8 border border-pink-300 rounded-lg w-11/12 sm:max-w-2xl lg:max-w-3xl mx-auto">
+    <div className="p-8 border-2 border-pink-300 rounded-lg w-11/12 sm:max-w-2xl lg:max-w-3xl mx-auto">
       <div>
-        <h3 className="text-2xl font-extrabold tracking-wide">
-          <span className="text-pink-500">{data.name}</span>, {data.sys?.country}
+        <h3 className="text-2xl font-extrabold tracking-wide text-gray-700">
+          <span className="text-pink-500">{data.name}</span>, {data.sys.country}
         </h3>
       </div>
       <div className="text-center">
         <Image
-          title={data.weather[0]?.description}
-          src={`http://openweathermap.org/img/wn/${data.weather[0]?.icon}@4x.png`}
+          title={data.weather[0].description}
+          src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`}
           height="200"
           width="200"
         />
-        <h4 className="text-4xl font-black tracking-wider text-pink-500 underline">{data.weather[0]?.main}</h4>
+        <h4 className="text-4xl font-black tracking-wider text-pink-500 underline">{data.weather[0].main}</h4>
         <ul className="mt-4">
-          <li className="text-2xl font-bold text-gray-800 my-1">
-            <span className="text-sm font-normal">(temp)</span> {KelvinToCelsius(data.main?.temp)} &#8451;
+          <li className="text-2xl font-extrabold text-gray-800 my-1">
+            <span className="text-sm font-normal">(temp)</span> {KelvinToCelsius(data.main.temp)} &#8451;
           </li>
-          <li className="text-2xl font-bold text-gray-800 my-1">
-            <span className="text-sm font-normal">(feels_like)</span> {KelvinToCelsius(data.main?.feels_like)} &#8451;
+          <li className="text-2xl font-extrabold text-gray-800 my-1">
+            <span className="text-sm font-normal">(feels_like)</span> {KelvinToCelsius(data.main.feels_like)} &#8451;
           </li>
         </ul>
       </div>
